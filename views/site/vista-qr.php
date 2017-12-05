3d7120add3b0518bea5e699de7905ba48aec9bae
AlfieElizondo <elizondo.alfie@gmail.com>
2017-12-05 11:31:49 -0600

    diseño de Vista QR

--- views/site/vista-qr.php
+++ views/site/vista-qr.php
@@ -2,5 +2,12 @@
 <?php
 
 ?>
+
+<div class="container container-premio">
+
 <h3>Gracias por participar</h3>
+<p>De resultar ganador te contactaremos para validar tu código y tu ticket de compra</p>
 <img src="<?= Yii::$app->urlManager->createAbsoluteUrl(['site/ver-codigo?token=' . $usuario->txt_token]) ?>" />
+
+</div>
+
