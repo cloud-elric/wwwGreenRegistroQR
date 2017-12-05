2f91cd42438c8d192db66ecc35280f3d739fe159
Humberto Antonio Marquez <humberto@2gom.com.mx>
2017-12-04 18:22:56 -0600

    Visualizacion de fecha

--- views/site/vista-datos.php
+++ views/site/vista-datos.php
@@ -1,4 +1,6 @@
 <?php
+
+use app\modules\ModUsuarios\models\Utils;
 echo $usuario->txt_nombre_completo;
 echo "<br>";
 echo $usuario->txt_telefono_celular;
@@ -7,5 +9,6 @@ echo $usuario->txt_email;
 echo "<br>";
 echo $usuario->num_pelotas;
 echo "<br>";
+echo Utils::changeFormatDate($usuario->fch_creacion);
 ?>
 
